bb88d10c46eaace9a4f4b992790933d6d402216a
Nameless-sama <anamelessdeath@gmail.com>
2017-10-16 14:11:29 -0500

    Se realizarón los cambios que permiten importar, guardar y eliminar tableros.

---
+++ Tabalim [Addin] [R22]/Runtime/Commands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabalim.Addin.Runtime
+{
+    /// <summary>
+    /// Define la clase de comandos
+    /// </summary>
+    public class Commands
+    {
+    }
+}
