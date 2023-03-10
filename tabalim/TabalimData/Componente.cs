d6cde920b7f23721013c68afa77129ff0c1f3dde
Miguel Alanis <anamelessdeath@gmail.com>
2023-03-10 17:31:51 -0600

    Tabalim data Proyecto exclusivo para manejar el acceso a datos a nivel de carga y guardado

---
+++ tabalim/TabalimData/Componente.cs
@@ -0,0 +1,13 @@
+﻿namespace Tabalim.Data
+{
+    public class Componente
+    {
+        public int comp_id { get; set; }
+        public int cir_id { get; set; }
+        public double potencia { get; set; }
+        public int img_index { get; set; }
+        public int comp_count { get; set; }
+        public string descripcion { get; set; }
+        public int delta_key { get; set; }
+    }
+}
---
+++ tabalim/TabalimData/TabalimCatalogos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabalim.Data
+{
+    public static class TabalimCatalogos
+    {
+        public static string TABLE_HP_WATTS { get => Catalogos.hp_watts; }
+    }
+}
